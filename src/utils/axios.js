61dbe852e17de14047425d8f06ccc22d189f4193
Mesbah-Himel <72723412+Mesbah-M13@users.noreply.github.com>
2023-01-29 16:54:02 +0600

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://json-api-fake-server-new.herokuapp.com/",
+    baseURL: "https://mern-json-api-server.onrender.com",
 });
 
 export default axiosInstance;
